ec265e79dd55218c61a31fc64b857cfbaa020a6f
Marcosplira <61232814+Marcosplira@users.noreply.github.com>
2023-10-17 15:04:17 -0300

    Add files via upload

---
+++ exemplo08.py
@@ -0,0 +1,81 @@
+# tenho_sede = True
+
+#if tenho_sede:
+#    print("beber agua")
+    
+
+# print("Vida que segue")
+
+
+# esta_frio = True
+# if esta_frio: 
+#     print("Vestir um casaco")
+# else: 
+#     print("Vestir camiseta")
+
+
+
+tenho_sede = True
+tenho_fome = False
+
+estou_em_dieta = False
+
+# if tenho_sede or tenho_fome: 
+#     print("vou na cozinha")
+# else: 
+#     print("ficar vendo netflix")
+
+
+# if tenho_sede and tenho_fome: 
+#     print("fazer sanduíche e coca")
+# else: 
+#     print("não tenho fome ou não tenho sede")
+
+
+# if tenho_sede and tenho_fome: 
+#     print("fazer sanduíche e coroa")
+# elif tenho_sede and not (tenho_fome): 
+#     if estou_em_dieta: 
+#         print("tomar agua")
+#     else:
+#         print("Tomar uma coquinha")
+# elif not (tenho_sede) and tenho_fome: 
+#     print("Fazer um sanduíche")
+# else: 
+#     print("ficar vendo netflix")
+
+
+
+
+num1 = 50
+num2 = 32
+
+# if num1 == num2:
+#     print("num1 é igual num2")
+# # elif num1 != num2:
+# #     print("num1 é diferente de num2")
+
+
+elif num1 > num2:
+    print("num1 é maior que num2")
+elif num1 < num2: 
+    print("num1 é menor que num2")
+
+# if num1 <= num2: 
+#     print("num1 é menor ou igual que num2")
+
+# Operadores Lógicos
+#  or => ou 
+# and => e
+#  not() = negação
+
+
+
+# Operadores de comparação
+# == -> igual
+# != -> diferente
+#  > -> maior
+#  < -> menor
+#  >= -> maior ou igual
+#  <= -> menor ou igual
+
